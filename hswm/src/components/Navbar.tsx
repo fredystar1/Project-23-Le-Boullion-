f3c97fcfa33f4fe9c9d885a060aab40a2a14b08b
brandon-was-here <brandonlafinch@gmail.com>
2026-02-22 22:33:57 -0600

    placeholder components & prelim tailwind css

--- hswm/src/components/Navbar.tsx
+++ hswm/src/components/Navbar.tsx
@@ -1,9 +1,31 @@
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>Hello World</h1>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/events">Events</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/wineclub">Wine Club</a>
+        </li>
+        <li>
+          <a href="/winedispensers">Wine Dispensers</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
